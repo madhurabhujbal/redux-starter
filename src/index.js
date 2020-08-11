90bf17d9e53dead3c38b108f1968a45f18541a95
Madhura <madhura.bhujbal@gmail.com>
2020-08-11 15:29:16 +0100

    Import compose and pipe objects from ramda package. Simplify code using compose function.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import { compose, pipe } from "ramda";
+
 let input = "  JavaScript  ";
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
 const wrapInDiv = (str) => `<div> ${str} </div>`;
 const toLowerCase = (str) => str.toLowerCase();
-const result = wrapInDiv(toLowerCase(trim(input)));
+
+const transform = compose(wrapInDiv, toLowerCase, trim);
+console.log(transform(input));
+//const result = wrapInDiv(toLowerCase(trim(input)));
