0c6feef39cd7cc02fe2dcca01901809585e2c589
Madhura <madhura.bhujbal@gmail.com>
2020-08-12 11:20:07 +0100

    Add function to wrap a function in span element

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ let input = "  JavaScript  ";
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
-const wrapInDiv = (str) => `<div> ${str} </div>`;
+const wrap = (type, str) => `<${type}> ${str} </${type}>`;
 const toLowerCase = (str) => str.toLowerCase();
 
 // const transform = compose(wrapInDiv, toLowerCase, trim);
-const transform = pipe(trim, toLowerCase, wrapInDiv);
+const transform = pipe(trim, toLowerCase, wrap);
 console.log(transform(input));
 //const result = wrapInDiv(toLowerCase(trim(input)));
