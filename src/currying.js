e037df40059235f6452d7714eef1bfcf4daf022e
Madhura <madhura.bhujbal@gmail.com>
2020-08-12 16:09:23 +0100

    Rearrange object updating code in seperate file

--- src/currying.js
+++ src/currying.js
@@ -33,27 +33,6 @@ val = "javascript";
 // console.log(wrapDiv(val));
 // console.log(wrapSpan(val));
 
-//Updating an object by Object.assign method
-const person = { name: "John" };
-const updated = Object.assign({}, person, { name: "Bob", age: 30 }); // copy person object in empty object and add name and age properties in new object.
-// console.log(updated);
-
-//Updating an object by spread operator
-const man = {
-  name: "John",
-  address: {
-    country: "USA",
-    city: "San Francisco",
-  },
-};
-const manupdt = {
-  ...man,
-  name: "Bob",
-  age: 30,
-  address: { ...person.address, city: "New York" },
-}; // copy man object with all its properties and add name and age properties in new object.
-//console.log(man);
-
 //Immutability with arrays:
 const numbers = [1, 2, 3];
 //Adding element in array
