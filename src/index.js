018bfbeec266c5fedb8e1775d9a6f5c1eb14c4ef
Madhura <madhura.bhujbal@gmail.com>
2020-08-13 11:52:03 +0100

    Add code to update JS object using immer library for immutability

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import { produce } from "immer";
 let book = { title: "Harry Potter" };
 
 function publish(book) {
-  book.isPublished = true;
+  return produce(book, (draftBook) => {
+    draftBook.isPublished = true;
+  }); //first arg is initial state of object; second arg  is function that takes original object and specifies mutations on it. produce() returns a new updated object
 }
 
-publish(book);
+let updated = publish(book);
 console.log(book);
+console.log(updated);
