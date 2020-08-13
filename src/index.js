41200b3ffb08c42778d34f2cee5727cece327c04
Madhura <madhura.bhujbal@gmail.com>
2020-08-13 11:20:04 +0100

    Add code to set object property using immutable library

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { Map } from "immutable";
 
 let book = Map({ title: "Harry Potter" });
 
-// function publish(book) {
-//   book.isPublished = true;
-// }
+function publish(book) {
+  return book.set("isPublished", true); //setting isPublished property using immutable library. This will return a new book object
+}
+book = publish(book);
 
-//publish(book);
 console.log(book.get("title")); //we cannot access book object properties by book.title. We have to use get().
 console.log(book.toJS()); //To convert immutable object to plain JS object, we have to call toJS()
