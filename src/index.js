1915b6c9b823bdca7ed8cc98d1feb4574863de16
Madhura <madhura.bhujbal@gmail.com>
2020-08-13 11:14:00 +0100

    Convert immutable object to JS object using toJS()

--- src/index.js
+++ src/index.js
@@ -10,3 +10,4 @@ let book = Map({ title: "Harry Potter" });
 
 //publish(book);
 console.log(book.get("title")); //we cannot access book object properties by book.title. We have to use get().
+console.log(book.toJS()); //To convert immutable object to plain JS object, we have to call toJS()
