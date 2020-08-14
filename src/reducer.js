e8ef59cafba780d45686dbab78b1220deacf5782
Madhura <madhura.bhujbal@gmail.com>
2020-08-14 12:03:23 +0100

    Modify the code for bug actions with switch-case statements

--- src/reducer.js
+++ src/reducer.js
@@ -2,17 +2,21 @@ let lastId = 0;
 
 function reducer(state = [], action) {
   //Setting initial state to [], else when app is started and Redux calls the reducer, state is undefined.
-  if (action.type === "bugAdded")
-    return [
-      ...state,
-      {
-        id: ++lastId,
-        description: action.payload.description,
-        resolved: false,
-      },
-    ];
-  else if (action.type === "bugRemoved")
-    return state.filter((bug) => bug.id !== action.payload.id);
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
 
-  return state;
+    case "bugRemoved":
+      return state.filter((bug) => bug.id !== action.payload.id);
+
+    default:
+      return state;
+  }
 }
