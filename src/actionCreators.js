09a37abbe75b93b0aa78f815a8062d2b977831ca
Madhura <madhura.bhujbal@gmail.com>
2020-08-14 16:10:46 +0100

    Call bugAdded function using an arrow function

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,10 +1,17 @@
 import * as actions from "./actionTypes"; //required only when creating an action
 
-export function bugAdded(description) {
-  return {
-    type: actions.BUG_ADDED,
-    payload: {
-      description: "Bug1",
-    },
-  };
-}
+export const bugAdded = (description) => ({
+  type: actions.BUG_ADDED,
+  payload: {
+    description: "Bug1",
+  },
+});
+
+// export function bugAdded(description) {
+//   return {
+//     type: actions.BUG_ADDED,
+//     payload: {
+//       description: "Bug1",
+//     },
+//   };
+// }
